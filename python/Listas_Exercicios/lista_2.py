7e2fb91008e1550da1498578278fce570d4b8ddd
Guilherme Leandrin Fernandes <guileandrin43@gmail.com>
2025-03-25 22:12:20 -0300

    Python_Lista_2

--- python/Listas_Exercicios/lista_2.py
+++ python/Listas_Exercicios/lista_2.py
@@ -59,3 +59,56 @@ else:
 # Exercício 4
 print("\n")
 print("Exercicio 4 - Identificação de Signo:") 
+nome = input("Digite seu nome: ")
+dia = int(input("Digite o dia em que você nasceu: "))
+mes = int(input("Digite o mes (numérico) em que você nasceu: "))
+ano = int(input("Digite o ano em que você nasceu: "))
+
+if ano <= 2025 and ano >= 1800:
+    if (mes == 1 and dia >= 20 and dia <= 31) or (mes == 2 and dia >= 1 and dia <= 18):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Aquário.")
+    elif (mes == 2 and dia >= 19 and dia <= 31) or (mes == 3 and dia >= 1 and dia <= 20):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Peixes.")
+    elif (mes == 3 and dia >= 21 and dia <= 31) or (mes == 4 and dia >= 1 and dia <= 19):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Áries.")
+    elif (mes == 4 and dia >= 20 and dia <= 31) or (mes == 5 and dia >= 1 and dia <= 20):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Touro.")
+    elif (mes == 5 and dia >= 21 and dia <= 31) or (mes == 6 and dia >= 1 and dia <= 20):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Gêmeos.")
+    elif (mes == 6 and dia >= 21 and dia <= 31) or (mes == 7 and dia >= 1 and dia <= 22):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Câncer.")
+    elif (mes == 7 and dia >= 23 and dia <= 31) or (mes == 8 and dia >= 1 and dia <= 22):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Leão.")
+    elif (mes == 8 and dia >= 23 and dia <= 31) or (mes == 9 and dia >= 1 and dia <= 22):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Virgem.")
+    elif (mes == 9 and dia >= 23 and dia <= 31) or (mes == 10 and dia >= 1 and dia <= 22):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Libra.")
+    elif (mes == 10 and dia >= 23 and dia <= 31) or (mes == 11 and dia >= 1 and dia <= 21):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Escorpião.")
+    elif (mes == 11 and dia >= 22 and dia <= 31) or (mes == 12 and dia >= 1 and dia <= 21):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Sagitário.")
+    elif (mes == 12 and dia >= 22 and dia <= 31) or (mes == 1 and dia >= 1 and dia <= 19):
+        print(f"Seu nome é {nome}, nasceu em {dia}/{mes}/{ano} e é de Capricórnio.")
+    else:
+        print("Data Inválida.")
+else:
+    print("Data Inválida.")
+
+
+# Exercício 5
+print("\n")
+print("Exercicio 5 - Cálculo de Premiação Salarial:") 
+salarioBase = float(input("Digite seu salário mensal: "))
+totalVendas = float(input("Digite o valor do total de vendas realizadas: "))
+
+if totalVendas < 1000:
+    valorPremiacao = 0
+elif totalVendas >= 1000 and totalVendas <= 5000:
+    valorPremiacao = 500
+elif totalVendas > 5000 and totalVendas <= 7500:
+    valorPremiacao = 750
+elif totalVendas > 7500:
+    valorPremiacao = 1000
+
+salarioFinal = salarioBase + valorPremiacao
+print(f"Salário Inicial: R${salarioBase:.2f} / Valor Premiação: R${valorPremiacao:.2f} / Salário Final: R${salarioFinal:.2f}")
